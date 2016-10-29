1f23a9eba57f598665df0efb4d201f05ffde088b
Andrew Carr <acarr@mail.utexas.edu>
2016-10-29 09:47:33 -0500

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express(); 
-var PORT = 3100; 
+var PORT = process.env.PORT || 3100; 
 
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
