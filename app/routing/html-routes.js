078f6b2027c50a819af48182d43430fd18bfbd25
Andrew Carr <acarr@mail.utexas.edu>
2016-10-29 09:33:53 -0500

    css fix

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -14,7 +14,7 @@ app.get('/survey', function (req, res) {
 	});
 
 	// If no matching route is found default to home
-	app.use(function (req, res) {
+	app.get('/', function (req, res) {
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
 	});
 };
